a58548f1f6b8356965ed5d6814699de3f7a08674
papochka2004 <fgdfgtgdjy@gmail.com>
2024-03-25 15:24:53 +0300

    sludr

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// // var name = 'hhk' + 88
-// // console.log(typeof name);
+
 // const melem = document.documentElement;
 // const melemW = melem.clientWidth
 // const melemH = melem.clientHeight
@@ -11,7 +10,24 @@ $(document).ready(function(){
         $('.header__burger,.header__body').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    $('.slider__body').slick();
+    $('.slider__body').slick({
+        slidesToShow: 3,
+        // variableWidth: true
+        responsive:[
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
     $('.header__link').click(function(event){
         $('.header__link').removeClass('active');
         $(this).addClass('active');
